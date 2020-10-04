82817fa4a4a5e9be7aaba83bfa7519b622e21750
Chivukula Sree Mrudula <chivukula.mrudula@gmail.com>
2020-10-04 20:11:47 +0530

    Added JUnit tests

--- src/test/java/com/crio/qcalc/QcalcApplicationTests.java
+++ src/test/java/com/crio/qcalc/QcalcApplicationTests.java
@@ -2,12 +2,20 @@ package com.crio.qcalc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class QcalcApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testAdd() {
+		StandardCalculator sCal = new StandardCalculator();
+        sCal.add(1,2);
+        assertEquals((long)3,(long)sCal.getResult());
 	}
 
 }
