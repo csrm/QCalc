61856a6138b6f7235c42641532b28a5c8e51eb26
Chivukula Sree Mrudula <chivukula.mrudula@gmail.com>
2020-10-05 13:29:25 +0530

    Reverted the Changes

--- src/test/java/com/crio/qcalc/CalcApplicationTests.java
+++ src/test/java/com/crio/qcalc/CalcApplicationTests.java
@@ -3,10 +3,9 @@ package com.crio.qcalc;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
-class QcalcApplicationTests {
+class CalcApplicationTests {
 
 	@Test
 	void contextLoads() {
